7bedb39761ba1001740f8476f176a70f99f1c599
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2024-02-12 21:46:32 +0100

    .

--- vite-project/src/constants.ts
+++ vite-project/src/constants.ts
@@ -1,6 +1,6 @@
 
 
-//export const BASE_URL = 'https://recipe-arnela.onrender.com'
+export const BASE_URL = 'https://recipe-arnela.onrender.com'
 
-export const BASE_URL = 'http://localhost:8080'
+//export const BASE_URL = 'http://localhost:8080'
 
